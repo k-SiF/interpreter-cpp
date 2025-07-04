6ba6bc560c4ecb8350121c5454c434eac8f94293
k-SiF <sifeddine.akaaboune03@gmail.com>
2025-07-04 18:14:46 +0200

    Added indentifier to the lexer

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,24 @@
 #include <format>
 #include <vector>
 #include <variant>
+#include <unordered_map>
 
 using Literal = std::variant<std::monostate, std::string, double, bool>;
 
 const int EXIT_LEXICAL_ERROR = 65;
+const int EXIT_PARSING_ERROR = 40;
 
 enum class ScanState {
-    NORMAL, STRING, COMMENT
+    NORMAL, STRING, COMMENT, NUMBER, IDENTIFIER
 };
 
 struct Token {
     enum class Type {
         LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, STAR,
-        DOT, COMMA, PLUS, MINUS, SEMICOLON, RETURN, EQUAL, EQUAL_EQUAL,
+        DOT, COMMA, PLUS, MINUS, SEMICOLON, ENTER, EQUAL, EQUAL_EQUAL,
         BANG, BANG_EQUAL, LESS, LESS_EQUAL, GREATER, GREATER_EQUAL,
-        SLASH, STRING,
+        SLASH, STRING, NUMBER, IDENTIFIER, AND, CLASS, ELSE, FALSE, FOR, 
+        FUN, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
         EOF_TOKEN
     };
     Type type;
@@ -46,12 +49,13 @@ std::vector<Token> lexer(const std::string& content, bool& err);
 std::string type_to_string(Token::Type t);
 Token* prev_token(std::vector<Token>& tokens);
 void replace_token(std::vector<Token>& tokens, const Token& token);
+char next_token(const std::string& tokens, const int index);
+const std::unordered_map<std::string, Token::Type>& get_keywords();
 
 int main(int argc, char *argv[]) {
     // Disable output buffering
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-
     
     if (argc < 3) {
         std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
@@ -60,6 +64,7 @@ int main(int argc, char *argv[]) {
     
     const std::string command = argv[1];
     
+    // LEXER
     if (command == "tokenize") {
         bool err = false;
         std::string file_contents = read_file_contents(argv[2]);
@@ -68,8 +73,8 @@ int main(int argc, char *argv[]) {
             std::vector<Token> tokens = lexer(file_contents, err);
 
             for (Token& token : tokens) {
-                if (!std::holds_alternative<std::string>(token.literal)) token.literal = std::string("null");
-                //else if (std::holds_alternative<double>(token.literal)) token.literal = std::to_string(token.literal);
+                if (std::holds_alternative<std::monostate>(token.literal)) token.literal = std::string("null");
+                //else if (std::holds_alternative<double>(token.literal)) token.literal = std::to_string(std::get<double>(token.literal));
                 std::cout << std::format("{} {} {}", type_to_string(token.type), token.lexeme, std::get<std::string>(token.literal)) << std::endl;
             }
 
@@ -78,6 +83,16 @@ int main(int argc, char *argv[]) {
             std::cout << "EOF  null" << std::endl;
         }
         
+    } 
+    // PARSER
+    else if (command == "parse") {
+        bool err = false;
+        std::string file_contents = read_file_contents(argv[2]);
+
+        if (!file_contents.empty()) {
+
+            if (err) return EXIT_PARSING_ERROR;
+        }
     } else {
         std::cerr << "Unknown command: " << command << std::endl;
         return EXIT_FAILURE;
@@ -90,33 +105,75 @@ std::vector<Token> lexer(const std::string& source, bool& err) {
     ScanState scan_state = ScanState::NORMAL;
     int line = 1;
     bool connected = true;
-    for (size_t i = 0; i < source.size(); ++i) {
+    int source_size = source.size();
+    for (size_t i = 0; i < source_size; ++i) {
         char c = source[i];
-        bool is_last = (i == source.size() - 1);
-        switch (scan_state)
-        {
-        case ScanState::COMMENT: {
-            if (c != '\n') continue;
-            scan_state = ScanState::NORMAL;
-            break;
-        }
-        case ScanState::STRING: {
-            if (c != '\"') {
-                if(is_last) {
-                    std::cerr << std::format("[line {}] Error: Unterminated string.", line) << std::endl;
-                    tokens.pop_back();
-                    err = true;
-                    continue;
-                }
-                if (tokens.empty() || prev_token(tokens)->type != Token::Type::STRING) tokens.push_back(Token{Token::Type::STRING, "", std::string(1, c), line});
-                else std::get<std::string>(prev_token(tokens)->literal).push_back(c);
-            } else {
-                prev_token(tokens)->lexeme = '\"' + std::get<std::string>(prev_token(tokens)->literal) + '\"';
+        bool is_last = (i == source_size - 1);
+
+        switch (scan_state) {
+            case ScanState::COMMENT: {
+                if (c != '\n') continue;
                 scan_state = ScanState::NORMAL;
+                break;
             }
-            continue;
-        }
-        default: break;
+            case ScanState::STRING: {
+                if (c != '\"') {
+                    if(is_last) {
+                        std::cerr << std::format("[line {}] Error: Unterminated string.", line) << std::endl;
+                        tokens.pop_back();
+                        err = true;
+                        continue;
+                    }
+                    if (tokens.empty() || prev_token(tokens)->type != Token::Type::STRING) tokens.push_back(Token{Token::Type::STRING, "", std::string(1, c), line});
+                    else std::get<std::string>(prev_token(tokens)->literal).push_back(c);
+                } else {
+                    prev_token(tokens)->lexeme = '\"' + std::get<std::string>(prev_token(tokens)->literal) + '\"';
+                    scan_state = ScanState::NORMAL;
+                }
+                continue;
+            }
+            case ScanState::NUMBER: {
+                Token* ptoken = prev_token(tokens);
+                std::string& literal = std::get<std::string>(ptoken->literal);
+                size_t dot = literal.find('.');
+                if (std::isdigit(c) || (c == '.' && dot == std::string::npos)) {  
+                    ptoken->lexeme.push_back(c);
+                    literal.push_back(c);
+                    if (!is_last) continue;
+                }
+                
+                if (dot != std::string::npos) {
+                    size_t end = literal.size() - 1;
+                    while (end > dot && literal[end] == '0') {
+                        --end;
+                    }
+
+                    if (end == dot) literal = literal.substr(0, end + 2);
+                    else literal = literal.substr(0, end + 1);
+                } else {
+                    literal.append(".0");
+                }
+
+                scan_state = ScanState::NORMAL; 
+                if (is_last && std::isdigit(c)) continue;
+                break;
+            }
+            case ScanState::IDENTIFIER: {
+                Token* ptoken = prev_token(tokens);
+                if (c != ' ' && (std::isalpha(c) || c == '_')) {
+                    ptoken->lexeme.push_back(c);
+                    if (!is_last) continue;
+                } 
+                const std::unordered_map<std::string, Token::Type> keywords = get_keywords();
+                auto it = keywords.find(ptoken->lexeme);
+                if (it != keywords.end()) {
+                    ptoken->type = it->second;
+                }
+                scan_state = ScanState::NORMAL;
+                if (is_last && (std::isalpha(c) || c == '_')) continue;
+                break;
+            }
+            default: break;
         }
 
         switch(c) {
@@ -156,11 +213,29 @@ std::vector<Token> lexer(const std::string& source, bool& err) {
             case '\n': connected = false; line++; break;
             case ' ': connected = false; break;
             case '\t': connected = false; break;
-            default: std::cerr << std::format("[line {}] Error: Unexpected character: {}", line, c) << std::endl; err = true; break;
+            default: {
+                if (std::isdigit(c)) { 
+                    tokens.push_back(Token{Token::Type::NUMBER, std::string(1, c), std::string(1, c), line});
+                    if (std::isdigit(next_token(source, i)) || next_token(source, i) == '.') scan_state = ScanState::NUMBER; 
+                    else std::get<std::string>(prev_token(tokens)->literal).append(".0");
+                } 
+
+                else if (std::isalpha(c) || c == '_') {
+                    tokens.push_back(Token{Token::Type::IDENTIFIER, std::string(1, c), line});
+                    if (std::isalpha(next_token(source, i))) scan_state = ScanState::IDENTIFIER;
+                }
+
+                else { std::cerr << std::format("[line {}] Error: Unexpected character: {}", line, c) << std::endl; err = true; }
+                break;
+            }
         }
     }
     tokens.push_back(Token{Token::Type::EOF_TOKEN, "", line});
     return tokens;
+}
+
+char next_token(const std::string& tokens, const int index) {
+    return tokens[index + 1];
 }
 
 void replace_token(std::vector<Token>& tokens, const Token& token) {
@@ -191,6 +266,28 @@ std::string read_file_contents(const std::string& filename) {
     return buffer.str();
 }
 
+const std::unordered_map<std::string, Token::Type>& get_keywords() {
+    static const std::unordered_map<std::string, Token::Type> keywords = {
+        {"and", Token::Type::AND},
+        {"class", Token::Type::CLASS},
+        {"else", Token::Type::ELSE},
+        {"false", Token::Type::FALSE},
+        {"for", Token::Type::FOR},
+        {"fun", Token::Type::FUN},
+        {"if", Token::Type::IF},
+        {"nil", Token::Type::NIL},
+        {"or", Token::Type::OR},
+        {"print", Token::Type::PRINT},
+        {"return", Token::Type::RETURN},
+        {"super", Token::Type::SUPER},
+        {"this", Token::Type::THIS},
+        {"true", Token::Type::TRUE},
+        {"var", Token::Type::VAR},
+        {"while", Token::Type::WHILE}
+    };
+    return keywords;
+}
+
 std::string type_to_string(Token::Type t) {
     switch (t) {
         case Token::Type::LEFT_PAREN: return "LEFT_PAREN";
@@ -213,7 +310,24 @@ std::string type_to_string(Token::Type t) {
         case Token::Type::GREATER_EQUAL: return "GREATER_EQUAL";
         case Token::Type::SLASH: return "SLASH";
         case Token::Type::STRING: return "STRING";
-        //case Token::Type::RETURN: return "RETURN";
+        case Token::Type::NUMBER: return "NUMBER";
+        case Token::Type::AND: return "AND";
+        case Token::Type::CLASS: return "CLASS";
+        case Token::Type::ELSE: return "ELSE";
+        case Token::Type::FALSE: return "FALSE";
+        case Token::Type::FOR: return "FOR";
+        case Token::Type::IDENTIFIER: return "IDENTIFIER";
+        case Token::Type::FUN: return "FUN";
+        case Token::Type::IF: return "IF";
+        case Token::Type::NIL: return "NIL";
+        case Token::Type::OR: return "OR";
+        case Token::Type::PRINT: return "PRINT";
+        case Token::Type::RETURN: return "RETURN";
+        case Token::Type::SUPER: return "SUPER";
+        case Token::Type::THIS: return "THIS";
+        case Token::Type::TRUE: return "TRUE";
+        case Token::Type::VAR: return "VAR";
+        case Token::Type::WHILE: return "WHILE";
         case Token::Type::EOF_TOKEN: return "EOF";
         default: return "UNKNOWN";
     }
